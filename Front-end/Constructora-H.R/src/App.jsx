c1c4051a87963a8cae57535e6a37fb730414235a
Matt3008c <carattmatt@gmail.com>
2026-09-07 14:58:44 -0500

    nuevas rutas de autorizaciones

--- Front-end/Constructora-H.R/src/App.jsx
+++ Front-end/Constructora-H.R/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
